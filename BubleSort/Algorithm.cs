a44e574bcd386384015616dff43a12eca1e2761b
bartlomiej.koceniak <bartlomiej.koceniak@E440>
2023-03-07 09:16:32 +0100

    Dodaj pliki projektów.

---
+++ BubleSort/Algorithm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubleSort
+{
+    internal class Algorithm
+    {
+        public void BubbleSort(int[] arr)
+        {
+            int temp = 0;
+            for (int write = 0; write < arr.Length; write++)
+            {
+                for (int sort = 0; sort < arr.Length - 1; sort++)
+                {
+                    if (arr[sort] > arr[sort + 1])
+                    {
+                        temp = arr[sort + 1];
+                        arr[sort + 1] = arr[sort];
+                        arr[sort] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
---
+++ BubleSort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BubleSort
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Algorithm algorithm = new Algorithm();
+            int[] tab1 = { 1, 2, 5, 3, 1, 7, 9, 1, 12, 83, 1, 5, 3, 2 };
+            int[] tab2 = { 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1 };
+            int[] tab3 = { 2, 2, 2, 2 };
+            int[] tab4 = { 1, 2 };
+            int[] tab5 = { 2, 1 };
+
+            algorithm.BubbleSort(tab1);
+            algorithm.BubbleSort(tab2);
+            algorithm.BubbleSort(tab3);
+            algorithm.BubbleSort(tab4);
+            algorithm.BubbleSort(tab5);
+        }
+    }
+}
